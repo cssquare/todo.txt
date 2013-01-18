87fa06cbfd27b820a9dc5a175cc0fd748ab187d0
Ian Bishop <bishop14@gmail.com>
2013-01-18 17:33:36 -0400

    Updated to use easy deploy for simple jar execution

--- src/main/java/org/cssquare/todo/App.java
+++ src/main/java/org/cssquare/todo/App.java
@@ -4,6 +4,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+      for(String arg : args)
+        System.out.println(arg);
     }
 }
